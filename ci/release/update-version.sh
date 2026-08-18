bf8afe5341c9b356032a0aeb38585dcccd4213e2
Bradley Dice <bdice@bradleydice.com>
2026-08-18 12:20:25 -0500

    Add devcontainer cache sources
    Signed-off-by: Bradley Dice <bdice@bradleydice.com>

--- ci/release/update-version.sh
+++ ci/release/update-version.sh
@@ -146,6 +146,7 @@ done
 # .devcontainer files
 find .devcontainer/ -type f -name devcontainer.json -print0 | while IFS= read -r -d '' filename; do
     sed_runner "s@rapidsai/devcontainers:[0-9.]*@rapidsai/devcontainers:${NEXT_SHORT_TAG}@g" "${filename}"
+    sed_runner "s@ghcr.io/nvidia/cuopt/devcontainer:[0-9.]*@ghcr.io/nvidia/cuopt/devcontainer:${NEXT_SHORT_TAG}@g" "${filename}"
     sed_runner "s@rapidsai/devcontainers/features/cuda:[0-9.]*@rapidsai/devcontainers/features/cuda:${NEXT_SHORT_TAG_PEP440}@" "${filename}"
     sed_runner "s@rapidsai/devcontainers/features/rapids-build-utils:[0-9.]*@rapidsai/devcontainers/features/rapids-build-utils:${NEXT_SHORT_TAG_PEP440}@" "${filename}"
     sed_runner "s@rapids-\${localWorkspaceFolderBasename}-[0-9.]*@rapids-\${localWorkspaceFolderBasename}-${NEXT_SHORT_TAG}@g" "${filename}"
